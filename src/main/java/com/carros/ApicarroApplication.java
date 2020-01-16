189b410980892b85cb529f88f1eafa6cd051896f
Edvaldo Santiago <edvaldo.santiago@gmail.com>
2020-01-16 00:30:15 -0300

    Persistencia

---
+++ src/main/java/com/carros/ApicarroApplication.java
@@ -0,0 +1,13 @@
+package com.carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicarroApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/carros/domain/dto/CarroDTO.java
@@ -0,0 +1,24 @@
+package com.carros.domain.dto;
+
+import org.modelmapper.ModelMapper;
+
+import com.carros.domain.Carro;
+
+import lombok.Data;
+
+@Data
+public class CarroDTO {
+	private Long id;
+	private String nome;
+	private String tipo;
+
+	/*
+	 * public CarroDTO(Carro carro) { this.id = carro.getId(); this.nome =
+	 * carro.getNome(); this.tipo = carro.getTipo(); }
+	 */
+
+	public static CarroDTO create(Carro carro) {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper.map(carro, CarroDTO.class);
+	}
+}
