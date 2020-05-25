596d803727b31f8065f2bd6c804fed415b5d6bef
Edvaldo S Matos <edvaldo.santiago@gmail.com>
2020-05-25 01:51:47 -0300

    Correção na classe de service e no dto

--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -22,49 +22,55 @@ public class CarroService {
 		return list;
 	}
 
-    public CarroDTO getCarroById(Long id) {
-        Optional<Carro> carro = rep.findById(id);
-        return carro.map(CarroDTO::consumeFromVO).orElseThrow(() -> new ObjectNotFoundException("Carro não encontrado"));
-    }
-    
-    public CarroDTO getCarroTipoNome(String tipo, String nome) {
-    	Optional<Carro> carro = rep.findByTipoAndNome(tipo, nome);
-    	return carro.map(CarroDTO::consumeFromVO).orElseThrow(() -> new ObjectNotFoundException("Carro não encontrado"));
-    }
-    
-    public List<CarroDTO> getCarrosByTipo(String tipo) {
-        return rep.findByTipo(tipo).stream().map(CarroDTO::consumeFromVO).collect(Collectors.toList());
-    }
+	public CarroDTO getCarroById(Long id) {
+		Optional<Carro> carro = rep.findById(id);
+		return carro.map(CarroDTO::consumeFromVO)
+				.orElseThrow(() -> new ObjectNotFoundException("Carro não encontrado"));
+	}
 
-    public CarroDTO insert(Carro carro) {
-        Assert.isNull(carro.getId(),"Não foi possível inserir o registro");
+	public CarroDTO getCarroTipoNome(String tipo, String nome) {
+		Optional<Carro> carro = rep.findByTipoAndNome(tipo, nome);
+		return carro.map(CarroDTO::consumeFromVO)
+				.orElseThrow(() -> new ObjectNotFoundException("Carro não encontrado"));
+	}
 
-        return CarroDTO.consumeFromVO(rep.save(carro));
-    }
+	public List<CarroDTO> getCarrosByTipo(String tipo) {
+		return rep.findByTipo(tipo).stream().map(CarroDTO::consumeFromVO).collect(Collectors.toList());
+	}
 
-    public CarroDTO update(Carro carro, Long id) {
-        Assert.notNull(id,"Não foi possível atualizar o registro");
+	public CarroDTO insert(Carro carro) {
+		Assert.isNull(carro.getId(), "Não foi possível inserir o registro");
 
-        // Busca o carro no banco de dados
-        Optional<Carro> optional = rep.findById(id);
-        if(optional.isPresent()) {
-            Carro db = optional.get();
-            // Copiar as propriedades
-            db.setNome(carro.getNome());
-            db.setTipo(carro.getTipo());
-            System.out.println("Carro id " + db.getId());
+		return CarroDTO.consumeFromVO(rep.save(carro));
+	}
 
-            // Atualiza o carro
-            rep.save(db);
+	public CarroDTO update(Carro carro, Long id) {
+		Assert.notNull(id, "Não foi possível atualizar o registro");
 
-            return CarroDTO.consumeFromVO(db);
-        } else {
-            return null;
-            //throw new RuntimeException("Não foi possível atualizar o registro");
-        }
-    }
+		// Busca o carro no banco de dados
+		Optional<Carro> optional = rep.findById(id);
 
-    public void delete(Long id) {
-        rep.deleteById(id);
-    }
+		if (optional.isPresent()) {
+
+			Carro db = optional.get();
+
+			// Copiar as propriedades
+			db.setNome(carro.getNome());
+			db.setTipo(carro.getTipo());
+
+			System.out.println("Carro id " + db.getId());
+
+			// Atualiza o carro
+			rep.save(db);
+
+			return CarroDTO.consumeFromVO(db);
+		} else {
+			throw new ObjectNotFoundException("Não foi possível atualizar o registro");
+			// throw new RuntimeException("Não foi possível atualizar o registro");
+		}
+	}
+
+	public void delete(Long id) {
+		rep.deleteById(id);
+	}
 }
--- src/main/java/com/carros/domain/dto/CarroDTO.java
+++ src/main/java/com/carros/domain/dto/CarroDTO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 @Data
 @Builder
 public class CarroDTO {
+	
 	private Long id;
 	private String nome;
 	private String tipo;
